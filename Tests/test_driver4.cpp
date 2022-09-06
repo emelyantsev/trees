afc9a1eb5cfe908fbb4edcf58060056268b47ba2
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-09-06 12:49:48 +0300

    Add benchmark tests

---
+++ Tests/test_driver4.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <set>
+
+#include "bst.h"
+#include "avl.h"
+
+int main(int argc, char *argv[]) {
+
+    int n = std::atoi(argv[1]) ;
+    int seed = std::atoi(argv[2]) ;
+    std::string type(argv[3]) ;
+    std::string mode(argv[4]) ;
+
+    std::cout << n << " " << seed << " " << type << std::endl;
+
+    if (type == "bst") {
+
+        BinarySearchTree bst;
+
+        srand(seed);
+
+        for (int i = 0; i < n; ++i) {
+
+            int newValue = mode == "random" ? rand() % n : i;
+        
+            BinarySearchTree::Node* newNode = bst.Insert(newValue);
+
+        }
+
+        std::vector<int> bstVector = bst.Print();
+
+        for (int item : bstVector ) {
+
+            BinarySearchTree::Node* nextNode = bst.Erase( item );
+
+        }
+    }
+
+    else if (type == "set") {
+
+        std::set<int> set;
+
+        srand(seed);
+
+        for (int i = 0; i < n; ++i) {
+
+            int newValue = mode == "random" ? rand() % n : i;
+            set.insert(newValue);
+
+        }
+
+        std::vector<int> setVector(set.begin(), set.end() );
+
+        for (int item : setVector ) {
+
+            set.erase( item );
+        }
+
+    }
+
+    else if (type == "avl") {
+
+        AvlTree avl;
+
+        srand(seed);
+
+        for (int i = 0; i < n; ++i) {
+
+            int newValue = mode == "random" ? rand() % n : i;
+        
+            AvlTree::Node* newNode = avl.Insert(newValue);
+
+        }
+
+        std::vector<int> avlVector = avl.Print();
+
+        for (int item : avlVector ) {
+
+            AvlTree::Node* nextNode = avl.Erase( item );
+
+        }
+    }
+
+    return 0;
+}
